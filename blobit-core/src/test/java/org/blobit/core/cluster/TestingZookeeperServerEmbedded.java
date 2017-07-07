a03bf27419dc8df9383b8118bdcc2f2772c48eb1
Enrico Olivelli <enrico.olivelli@diennea.com>
2017-07-07 15:04:01 +0200

    disable zk http admin api

--- blobit-core/src/test/java/org/blobit/core/cluster/TestingZookeeperServerEmbedded.java
+++ blobit-core/src/test/java/org/blobit/core/cluster/TestingZookeeperServerEmbedded.java
@@ -36,6 +36,10 @@ import org.apache.zookeeper.server.quorum.QuorumPeerMain;
  */
 public class TestingZookeeperServerEmbedded implements AutoCloseable {
 
+    static {
+        System.setProperty("zookeeper.admin.enableServer", "false");
+    }
+
     QuorumPeerConfig config;
     QuorumPeerMain maincluster;
     ZooKeeperServerMain mainsingle;
